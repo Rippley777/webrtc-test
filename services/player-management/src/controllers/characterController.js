465f34895d1862a4eb8f92dba03726980aff7db8
Ally Rippley <ally.rippley@gmail.com>
2024-05-18 01:22:55 -0500

    add character limit

--- services/player-management/src/controllers/characterController.js
+++ services/player-management/src/controllers/characterController.js
@@ -2,10 +2,10 @@ const { validationResult } = require("express-validator");
 const db = require("../db");
 
 exports.createCharacter = async (req, res) => {
+  console.log("/create-character", { body: req.body });
   const userId = req.auth.userId; // Assuming userId is stored in the JWT payload
 
   async (req, res) => {
-    // Check validation results
     const errors = validationResult(req);
     const { name } = req.body;
     if (!errors.isEmpty()) {
@@ -16,16 +16,30 @@ exports.createCharacter = async (req, res) => {
       const player = await db.query("SELECT * FROM players WHERE userId = $1", [
         userId,
       ]);
+
       if (!player) {
         return res.status(404).send("Player not found");
       }
+
+      const existingCharacter = await db.query(
+        "SELECT * FROM characters WHERE player_id = $1",
+        [player.playerId]
+      );
+
+      if (existingCharacter.rows.length > 0) {
+        return res
+          .status(409)
+          .send("You've reached the current max number of characters");
+      }
+
       console.log("/player/create-character", { player });
-      console.log("/create-player", { player });
+
       const result = await db.query(
         `INSERT INTO characters (player_id, name, level, experience_points, health, inventory, skills) 
        VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
         [player.playerId, name, 1, 0, 100, "{}", "{}"]
       );
+
       res.status(201).json(result.rows[0]);
     } catch (error) {
       console.error("/create-character", { error });
