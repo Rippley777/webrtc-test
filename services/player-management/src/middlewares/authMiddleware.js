431ea9e39d8f7c933355366bed9626205d2cd541
Ally Rippley <ally.rippley@gmail.com>
2024-05-18 13:54:51 -0500

    add logging to auth middleware

--- services/player-management/src/middlewares/authMiddleware.js
+++ services/player-management/src/middlewares/authMiddleware.js
@@ -1,8 +1,9 @@
 const jwt = require("jsonwebtoken");
+const logger = require("../lib/helpers/logger");
 
 exports.requireAuth = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  console.log("/player requireAuth middleware hit", { token });
+  logger.info("/player requireAuth [middleware]", { token });
 
   if (!token) {
     return res.status(401).send("No token provided");
@@ -12,6 +13,7 @@ exports.requireAuth = (req, res, next) => {
       return res.status(401).send("Invalid token");
     }
     req.auth = payload;
+    logger.success("/player requireAuth middleware", { payload });
     next();
   });
 };
