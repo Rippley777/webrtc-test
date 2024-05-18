ed73cd0e9c30eeb2c680b0e0a49248b35d4587e8
Ally Rippley <ally.rippley@gmail.com>
2024-05-18 18:58:11 -0500

    add cors to world server, update nginx route

--- services/world-server/src/index.js
+++ services/world-server/src/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const redis = require("redis");
 const logger = require("./lib/helpers/logger");
+const cors = require("cors");
 
 // Create Express app
 const app = express();
 
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Allow cookies to be sent with requests
+  optionsSuccessStatus: 204,
+};
+
 // Middleware setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
 
 // Import routes
 const locationRoutes = require("./routes/locationRoutes");
