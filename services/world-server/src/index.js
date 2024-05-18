fd97c7fa11b60d81f482825d03103bd63a780e6a
Ally Rippley <ally.rippley@gmail.com>
2024-05-18 18:45:44 -0500

    update to world server

--- services/world-server/src/index.js
+++ services/world-server/src/index.js
@@ -10,11 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Create Redis client
-const redisClient = redis.createClient();
-redisClient.on("error", (err) => logger.error("Redis error", { error: err }));
-redisClient.on("connect", () => logger.info("Connected to Redis"));
-
 // Import routes
 const locationRoutes = require("./routes/locationRoutes");
 app.use("/locations", locationRoutes);
