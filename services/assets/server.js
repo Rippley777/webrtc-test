53d3087ef8d1327eda88d3ed1fa9b46e2f4bb46c
Ally Rippley <ally.rippley@gmail.com>
2024-05-20 20:11:26 -0500

    add cors to assets service

--- services/assets/server.js
+++ services/assets/server.js
@@ -1,18 +1,26 @@
 // server.js
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const port = 3001;
 
-// Serve static files from the assets directory
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Allow cookies to be sent with requests
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-  res.send('Asset Service is running.');
+// Serve static files from the assets directory
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+app.get("/", (req, res) => {
+  res.send("Asset Service is running.");
 });
 
 app.listen(port, () => {
   console.log(`Asset service running at http://localhost:${port}`);
 });
-
